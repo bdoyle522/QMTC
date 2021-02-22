1477a035a704934a1549923ac6ae9ea42385eb21
Brendan Doyle <bdoyle522@gmail.com>
2021-02-22 09:51:15 -0700

    add back input validator

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -14,6 +14,7 @@ export const UserInput = ({ inputType, value, onChange, placeholder }) => (
     onChange={onChange}
     value={value}
     placeholder={placeholder}
+    type={inputType === NUMBER ? 'number' : undefined}
   />
 );
 
